c206e813284b29fad111d99f5a340fdec0e44d6f
Yumingz <yzhang@cs.dal.ca>
2014-09-30 20:04:33 +0000

    added CPU Type

--- uname.c
+++ uname.c
@@ -22,6 +22,7 @@ int main(int argc, char *argv[]) {
   	printf("Host name: %s\n", ubuffer.nodename);  
  	printf("OS Release: %s\n", ubuffer.release); 
 	printf("OS Version: %s\n", ubuffer.version);
+    	printf("CPU Type: %s\n", ubuffer.machine);
     }
     return 0;
 }
